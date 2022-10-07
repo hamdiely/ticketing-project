f987b161ec532b2ff19063052b36594c024086de
mohamedabdalla <hamdiely@gmail.com>
2022-10-07 16:03:39 -0400

    update button in user create

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -6,10 +6,7 @@ import com.cydeo.service.UserService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -45,4 +42,17 @@ public class UserController {
         return "redirect:/user/create";
 
     }
+@GetMapping("/update/{username}")
+    public String editUser(@PathVariable("username") String username,  Model model){
+
+        model.addAttribute("user", userService.findById(username));
+
+        model.addAttribute("roles",roleService.findAll());
+
+        model.addAttribute("users", userService.findAll());
+        return"/user/update";
+    }
+
+
+
 }
