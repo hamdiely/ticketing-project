19dcdfe2ba64efbe7b92a1e2cae7aaedf3384655
Mohamed <hamdiely@gmail.com>
2022-10-29 17:09:08 -0400

    Task create button p1

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.UserService;
@@ -49,6 +50,25 @@ public class ProjectController {
     public String completeProject(@PathVariable("projectCode") String projectCode){
 
           projectService.complete(projectService.findById(projectCode));
+        return "redirect:/project/create";
+    }
+
+
+    @GetMapping("/update/{projectCode}")
+    public String editProject(@PathVariable("projectCode") String projectCode,  Model model){
+
+        model.addAttribute("project", projectService.findById(projectCode));
+
+        model.addAttribute("managers",userService.findManagers());
+
+        model.addAttribute("projects", projectService.findAll());
+        return"/project/update";
+    }
+    @PostMapping("/update")
+    public String updateProject(@ModelAttribute("project") ProjectDTO project){
+
+        projectService.update(project);
+
         return "redirect:/project/create";
     }
 
--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -32,6 +32,10 @@ public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String> i
 
     @Override
     public void update(ProjectDTO object) {
+
+        if(object.getProjectStatus()==null){
+            object.setProjectStatus(findById(object.getProjectCode()).getProjectStatus());
+        }
         super.update(object.getProjectCode(), object);
 
     }
